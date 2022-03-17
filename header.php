a12de0301526e62038471da1125e4a498903da00
sprosenblum <81576433+sprosenblum@users.noreply.github.com>
2022-03-17 12:33:53 -0400

    Update index.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
   
   <body>
 
-  <header>
+    <header>
       <h1>Mama Mia Pizzeria</h1>
     </header>
     
@@ -28,10 +28,13 @@
       <a class="navbar-brand" href="#">
         <img src="https://i.picsum.photos/id/155/100/100.jpg?hmac=B81g8yBckUoQbJmgcZna0gcebXS72bMJO4Bsas71RIY">
       </a>
+
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarText">
+        
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="#menu-section">Menu</a>
@@ -51,7 +54,8 @@
           <li class="nav-item">
             <a class="nav-link" href="#contact">Contact</a>
           </li>
-        </ul> 
+        </ul>
+
       </div>
     </div>
   </nav>
--- index.php
+++ index.php
@@ -1,232 +1,252 @@
 <?php get_header(); ?>
 
-  <main>
-    <div class="container">
-      <h1 class="mt-5 mb-5 text-center">MENU</h1>
+<main>
+  
+  <div class="container"> <!-- missing closing? -->
+    <h1 class="mt-5 mb-5 text-center">MENU</h1>
 
     <hr>
 
-      <div class="row">
-        <div class="col-12 offset-md-4 col-md-6 text-center text-md-start">
-          <h2 class="cost mb-5">Pizzas - cost per slice</h2>
-        </div>
+    <div class="row">
+      <div class="col-12 offset-md-4 col-md-6 text-center text-md-start">
+        <h2 class="cost mb-5">Pizzas - cost per slice</h2>
       </div>
+    </div>
 
-    
+  
     <?php 
     $loop = new WP_Query( array( 'post_type' => 'pizzas', 'posts_per_page' => 10 ) ); 
 
     while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
       <div class="row mb-5">
-      <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
+        <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
+          <img 
+            class="pizza-item-img mb-2"
+            src="<?php echo get_the_post_thumbnail_url(); ?>"
+            alt="Food Image"
+          />
+        </div>
 
-        <img class="pizza-item-img mb-2"
-          src="<?php echo get_the_post_thumbnail_url(); ?>"
-          alt="Food Image"
-        />
-      </div>
+        <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
+          <h3 class="menu-item-heading">
+            <span class="menu-item-name">
+              <?php the_title(); ?> 
+            </span>
 
-      <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
-        <h3 class="menu-item-heading">
-        <span class="menu-item-name">
-        <?php the_title(); ?> 
-      </span>
-
-          <!-- render price onto page -->
-          <span class="menu-item-price"> $<?php echo get_post_meta($post->ID, 'price', true); ?>
-          </span>
+            <!-- render price onto page -->
+            <span class="menu-item-price"> 
+              &nbsp;$<?php echo get_post_meta($post->ID, 'price', true); ?>
+            </span>
           </h3>
-  
-    <p class="mt-3">
-      <?php the_content(); ?>
-    </p>
-    </div>
-  </div>
+
+          <p class="mt-3">
+            <?php the_content(); ?>
+          </p>
+        </div>
+      </div>
 
     <?php endwhile; ?> <!-- end pizza section -->
 
     <hr>
 
-      <!-- sauces -->
+    <!-- sauces -->
 
     <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="mb-5 font-weight-bold">Sauces</h2>
-        </div>
+      <div class="col-12 text-center">
+        <h2 class="mb-3 font-weight-bold">Sauces</h2>
       </div>
+    </div>
 
-          <?php 
+    <?php 
 
-          $loop = new WP_Query( array( 'post_type' => 'sauces', 'posts_per_page' => 10 ) ); 
-
-          while ( $loop->have_posts() ) : $loop->the_post(); ?>
-
-            <div class="container">
-              <div class="row">
-                <div class="col-sm-2">
-                  <div class="text-sm-end"><strong><?php the_title(); ?></strong></div>
-              </div>
-            <div class="col-sm-10">
-              <?php the_content(); ?>
-            </div>
-            </div>
-          </div>
-
-            <?php endwhile; ?> <!-- end sauces -->
-
-            <hr>
-
-      <!-- Toppings -->
-
-      <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="mb-3 font-weight-bold">Toppings</h2>
-        </div>
-      </div>
-
-      <?php 
-        $loop = new WP_Query( array( 'post_type' => 'toppings', 'posts_per_page' => 10 ) ); 
-
-        while ( $loop->have_posts() ) : $loop->the_post(); ?>
-
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <div class="text-center"><?php the_title(); ?></div>
-            </div>
-          </div>
-        </div>
-
-          <?php endwhile; ?>  <!-- end toppings -->
-
-    <hr>
-
-      <!-- sides -->
-      <div class="row">
-        <div class="col-12 offset-md-4 col-md-6 text-center text-md-start">
-          <h2 class="mb-5">Sides - cost per item</h2>
-        </div>
-      </div>
-
-      <?php 
-    $loop = new WP_Query( array( 'post_type' => 'sides', 'posts_per_page' => 10 ) ); 
+    $loop = new WP_Query( array( 'post_type' => 'sauces', 'posts_per_page' => 10 ) ); 
 
     while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-      <div class="row mb-5">
-      <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
-        <img class="sides-item-img mb-2"
-          src="<?php echo get_the_post_thumbnail_url(); ?>"
-          alt="Food Image"
-        />
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="text-sm-end">
+              <strong><?php the_title(); ?></strong>
+            </div>
+          </div>
+
+          <div class="col-sm-10">
+            <?php the_content(); ?>
+          </div>
+
+        </div>
       </div>
 
-      <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
-        <h3 class="menu-item-heading">
-        <span class="menu-item-name">
-        <?php the_title(); ?> 
-      </span>
+    <?php endwhile; ?> <!-- end sauces -->
 
-          <!-- render price onto page -->
-          <span class="menu-item-price"> $<?php echo get_post_meta($post->ID, 'price', true); ?>
-          </span>
-          </h3>
-  
-    <p class="mt-3">
-      <?php the_content(); ?>
-    </p>
+    <hr>
+
+    <!-- Toppings -->
+
+    <div class="row">
+      <div class="col-12 text-center">
+        <h2 class="mb-3 font-weight-bold">Toppings</h2>
+      </div>
     </div>
-  </div>
+
+    <?php 
+      $loop = new WP_Query( array( 'post_type' => 'toppings', 'posts_per_page' => 10 ) ); 
+
+      while ( $loop->have_posts() ) : $loop->the_post(); 
+
+    ?>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="text-center">
+              <?php the_title(); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <?php endwhile; ?>  <!-- end toppings -->
+
+    <hr>
+
+    <!-- sides -->
+    <div class="row">
+      <div class="col-12 offset-md-4 col-md-6 text-center text-md-start">
+        <h2 class="mb-5">Sides - cost per item</h2>
+      </div>
+    </div>
+
+    <?php 
+      $loop = new WP_Query( array( 'post_type' => 'sides', 'posts_per_page' => 10 ) ); 
+
+      while ( $loop->have_posts() ) : $loop->the_post(); 
+      
+    ?>
+
+      <div class="row mb-5">
+        <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
+          <img class="sides-item-img mb-2"
+            src="<?php echo get_the_post_thumbnail_url(); ?>"
+            alt="Food Image"
+          />
+        </div>
+
+        <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
+          <h3 class="menu-item-heading">
+            <span class="menu-item-name">
+              <?php the_title(); ?> 
+            </span>
+
+            <!-- render price onto page -->
+            <span class="menu-item-price">
+              &nbsp;$<?php echo get_post_meta($post->ID, 'price', true); ?>
+            </span>
+          </h3>
+
+          <p class="mt-3">
+            <?php the_content(); ?>
+          </p>
+        </div>
+      </div>
 
     <?php endwhile; ?>   <!-- end sides section -->
 
-      <hr>
+    <hr>
 
     <div class="row">
-        <h2 id="order-now" class="col-12 text-center mt-5 mb-5"><a href="">Order Your Hot Pizza Now!</a></h2>
+      <h2 id="order-now" class="col-12 text-center mt-5 mb-5">
+        <a href="">Order Your Hot Pizza Now!</a>
+        <!-- @todo add a link -->
+      </h2>
     </div>
 
     <hr>
 
-      <!-- Location Section -->
-<div class="row" id="locations">
-    <div class="col-12 text-center">
-      <h2 class="mb-5 font-weight-bold">Locations</h2>
+    <!-- Location Section -->
+    <div class="row" id="locations">
+      <div class="col-12 text-center">
+        <h2 class="mb-5 font-weight-bold">Locations</h2>
+      </div>
     </div>
-  </div>
 
-      <?php 
+    <?php 
 
       $loop = new WP_Query( array( 'post_type' => 'locations', 'posts_per_page' => 10 ) ); 
 
-      while ( $loop->have_posts() ) : $loop->the_post(); ?>
-
+      while ( $loop->have_posts() ) : $loop->the_post(); 
+    
+    ?>
 
         <div class="container">
 
           <div class="row">
             <div class="col-sm-2">
-              <div class="text-sm-end"><strong><?php the_title(); ?></strong></div>
+              <div class="text-sm-end">
+                <strong><?php the_title(); ?></strong>
+              </div>
+            </div>
+
+            <div class="col-sm-10">
+              <?php the_content(); ?>
+            </div>
+      
+          <!-- rendering map from advanced custom fields -->
+          <?php 
+          $map = get_field('map');
+          if( $map ): ?>
+              <a class="button text-center mb-5" href="<?php echo esc_url( $map ); ?>">Find Us!</a>
+          <?php endif; ?>
+          <!-- end map -->
+
           </div>
-        <div class="col-sm-10">
+        </div>
+
+    <?php endwhile; ?> <!-- End Location Section -->
+        
+    <hr>
+
+    <!-- story -->
+
+    <?php 
+    $loop = new WP_Query(array('post_type' => 'our_story', 'posts_per_page' => 1)); 
+
+    while ($loop->have_posts()) : $loop->the_post(); ?>
+
+    <div id="page-story" class="mt-3 row">
+      <h3 class="mb-4 text-center col-12">Our Story</h3>
+        <article class="mb-3 col-12 offset-md-1 col-md-10 text-center text-md-start">
           <?php the_content(); ?>
-        </div>
-       
-        <!-- rendering map from advanced custom fields -->
-        <?php 
-        $map = get_field('map');
-        if( $map ): ?>
-            <a class="button text-center mb-5" href="<?php echo esc_url( $map ); ?>">Find Us!</a>
-        <?php endif; ?>
-        <!-- end map -->
-
-        </div>
-      </div>
-
-        <?php endwhile; ?> <!-- End Location Section -->
-          
-        <hr>
-
-<!-- story -->
-
-<?php 
-$loop = new WP_Query(array('post_type' => 'our_story', 'posts_per_page' => 1)); 
-
-while ($loop->have_posts()) : $loop->the_post(); ?>
-
-<div id="page-story" class="mt-3 row">
-        <h3 class="mb-4 text-center col-12">Our Story</h3>
-    <article class="mb-3 col-12 offset-md-1 col-md-10 text-center text-md-start">
-      <?php the_content(); ?>
-  </article>
-  </div>
-
-  <?php endwhile; ?> <!-- end story -->
-
-  <hr>
-
-  <!-- contact section -->
-
-  <?php 
-$loop = new WP_Query(array('post_type' => 'contact', 'posts_per_page' => 1)); 
-
-while ($loop->have_posts()) : $loop->the_post(); ?>
-
-<div id="contact" class="mt-5 row">
-        <h3 class="mb-3 text-center col-12">Contact Us</h3>
-    <article class="mb-5 col-12 offset-md-1 col-md-10 text-center">
-      <?php the_content(); ?>
-  </article>
-  </div>
-
-  <?php endwhile; ?>  <!-- end contact section -->
-
-
-    </main>
+        </article>
     </div>
-  </div>
 
-  </body>
+    <?php endwhile; ?> <!-- end story -->
+
+    <hr>
+
+    <!-- contact section -->
+
+    <?php 
+    $loop = new WP_Query(array('post_type' => 'contact', 'posts_per_page' => 1)); 
+
+    while ($loop->have_posts()) : $loop->the_post(); ?>
+
+    <div id="contact" class="mt-5 row">
+      <h3 class="mb-3 text-center col-12">Contact Us</h3>
+      <article class="mb-5 col-12 offset-md-1 col-md-10 text-center">
+        <?php the_content(); ?>
+      </article>
+    </div>
+
+    <?php endwhile; ?>  <!-- end contact section -->
+
+</main>
+
+
+<!-- </div> div started in header.php
+</div> div started in header.php -->
+
+</body> <!-- body started in header.php -->
 
 <?php get_footer(); ?>  
