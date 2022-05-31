5bd282b782f0821203b1d31ce88468d5055850d4
Ryan Mayo <81576433+sprosenblum@users.noreply.github.com>
2022-05-31 13:28:16 -0400

    combining bootstrap with popper cdn

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function pizzeria_register_styles() {
 
     wp_register_style(
-        'Bootstrap_5', 
+        'Bootstrap_5',
         'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
     );
 
@@ -11,7 +11,7 @@ function pizzeria_register_styles() {
 
     wp_enqueue_style('Theme_Styles', get_template_directory_uri() . '/style.css');
 
-    wp_enqueue_script('Popper','https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js', array('jquery'), '', true);
+    wp_enqueue_script('Bootstrap_with_Popper','https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
 }
 
 add_action('wp_enqueue_scripts', 'pizzeria_register_styles');
