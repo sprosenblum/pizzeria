d4057ac80fd675a3c8ecb949112c43122fbde25c
Ryan Mayo <81576433+sprosenblum@users.noreply.github.com>
2022-06-02 11:24:42 -0400

    updating

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <div class="row justify-content-center">
         <div class="col-md-4" id="hours">
           Hours <br />
-      
+
           Mon - Thu : 12 PM - 9 PM <br />
           Friday - Sat : 12 PM - 12 AM <br />
           Sun : 12 PM - 10 PM
@@ -12,12 +12,12 @@
           Serving the Greater Columbus Area!
         </div>
       </div>
-    </div> 
+    </div>
 
 
   <div class="footer-bottom mt-5">
-    <div class="site-by white"> 
-      <?php echo (new DateTime)->format('Y'); ?> Site &copy; Mama Mia Pizzeria - by 
+    <div class="site-by white">
+      <?php echo (new DateTime)->format('Y'); ?> &copy; Mama Mia Pizzeria - Site by
       <a href="#" target="_blank">Sarah Rosenblum</a>
     </div>
   </div>
